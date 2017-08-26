daeb2bc917cfe8c49e33a05e83071e0688788727
qq79402005@gmail.com <qq79402005@gmail.com>
2017-08-26 23:23:23 +0800

    完形填空

--- client/templates/bankedcloze/BankedCloze.gd
+++ client/templates/bankedcloze/BankedCloze.gd
@@ -2,13 +2,31 @@ extends Node
 
 var question_file = "res://data/test.blankedcloze.xml"
 var question_data = {}
+var content_now   = ""
 
 func _ready():
 	load_question()
-	get_node("content").set_text(question_data.content)
+	
+	content_now = question_data.content
+	for i in range(20):
+		var orig_str = "(%d)" % i 
+		var dest_str = "[color=red][url=%d](%d)[/url][/color]" % [i, i]
+		content_now = content_now.replace(orig_str, dest_str)
+	
+	get_node("content").set_use_bbcode(true)
+	get_node("content").append_bbcode(content_now)
 	
 func load_question():
 	var file = File.new()
 	file.open(question_file,File.READ)
 	var json_str = file.get_as_text()
 	question_data.parse_json(json_str) 
+
+func _on_content_meta_clicked( meta):
+	var i = int(meta)
+	var orig_str = "[color=red][url=%d](%d)[/url][/color]" % [i, i]
+	var dest_str = "[color=green][url=%d](Apple)[/url][/color]" % i
+	content_now = content_now.replace(orig_str, dest_str)
+	
+	get_node("content").clear()
+	get_node("content").append_bbcode(content_now)
