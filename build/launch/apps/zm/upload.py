3b45180bfe6a16cf1084647f4926b830978396c3
Albert Cameron <qq79402005@gmail.com>
2017-09-29 17:25:48 +0800

    统一更新流程

--- build/launch/apps/zm/upload.py
+++ build/launch/apps/zm/upload.py
@@ -14,6 +14,13 @@ os.system("python gen_version_meta.py")
 # upload
 oss2.resumable_upload(bucket, "Software/zm/update/version.meta", "version.meta", multipart_threshold=100*1024)
 print("upload file [version.meta] to ALIYUN oss")
-oss2.resumable_upload(bucket, "Software/zm/update/data.pck", "data.pck", multipart_threshold=100*1024)
-print("upload file [data.pck] to ALIYUN oss")
-print("upload files finished")
+
+# upload pcks
+root_path = os.getcwd() + '/'
+dirs = os.listdir(root_path)
+dirs.sort()
+for file in dirs:
+    if os.path.splitext(file)[1] == '.pck':
+        oss2.resumable_upload(bucket, "Software/zm/update/" + file, file, multipart_threshold=100*1024)
+        print("upload file [%s] to ALIYUN oss" % file)
+        print("upload files finished")
