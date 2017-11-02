f3342bc62529b2e9dc2c08709078ffad48ba1577
Albert Cameron <qq79402005@gmail.com>
2017-11-02 21:07:03 +0800

    使用native方法播放mp4

--- client/templates/video/videoplay.gd
+++ client/templates/video/videoplay.gd
@@ -4,12 +4,19 @@ var exercises = null
 var exercise_data = {}
 var is_answer_right = false
 
+func _ready():
+	set_process(true)
+	
+func _process(delta):
+	if exercises!=null and not is_hidden():
+		if not OS.native_video_is_playing():
+			exercises.next_exercise(true)
+
 func set_data(data, exercises_):
 	exercise_data = data
 	exercises = exercises_
 	
+	OS.native_video_play(exercise_data.url, 1.0, "", "")
+	
 func _on_return_pressed():
 	get_node("/root/launch").show("lessons")
-
-func _on_continue_pressed():
-	exercises.next_exercise(is_answer_right)
